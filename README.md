f58016845b537de8238c3f8962d7a274f82e741f
alfarttusie <m.alfarttusie@gmail.com>
2024-12-03 19:01:46 +0300

    update to css instead of javascript

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 show Nvidia Notification with html css
 with html css and javascript can create Notification like nvidia GeForce
 
-codepen : https://codepen.io/alfarttusie/pen/vYMWBdP
+codepen : https://codepen.io/alfarttusie/pen/emOpvPa
+
+* add the css code to your code
+* add the javascript function
+* enjoe ...use it like showNotification(some text)
+
--- js.js
+++ js.js
@@ -1,25 +1,17 @@
-async function showNotification(message) {
-  function delay(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
-  try {
-    message = message === null ? "رسالة فارغة" : message;
-    let notificationHolder = document.querySelector(".Notify");
-    let secondaryText = notificationHolder.querySelector(".secondary");
-    let messageText = notificationHolder.querySelector(".Text");
-
-    notificationHolder.style.width = "240px";
-    await delay(250).then();
-    secondaryText.style.width = "95%";
-    await delay(250).then();
-    await delay(150).then();
-    messageText.textContent = message;
-    await delay(500).then();
-    messageText.textContent = "";
-    secondaryText.style.width = "0px";
-    await delay(250).then();
-    notificationHolder.style.width = "0px";
-  } catch (error) {
-    console.log(`Tools -> showNotification -> ${error}`);
-  }
+function showNotification(Text) {
+  const Holder = document.createElement("div");
+  Holder.classList.add("Notify");
+  const Secondary = document.createElement("div");
+  Secondary.classList.add("secondary");
+  const span = document.createElement("span");
+  span.classList.add("Text");
+  Holder.appendChild(Secondary);
+  Secondary.appendChild(span);
+  document.body.appendChild(Holder);
+  Holder.classList.add("active");
+  span.innerText = Text;
+  setTimeout(() => {
+    Holder.classList.remove("active");
+    Holder.remove();
+  }, 2500);
 }
